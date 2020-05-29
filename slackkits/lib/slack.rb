9efdfdabe7da927f3082b3c957c3a18baf1e7530
s-tajima <tajima1989@gmail.com>
2020-05-29 22:08:33 +0900

    Add slackkits

---
+++ slackkits/lib/slack.rb
@@ -0,0 +1,37 @@
+def gen_client(api_key)
+  Slack.configure do |c|
+    c.token = api_key
+  end
+
+  Slack::Web::Client.new
+end
+
+
+def ids_to_emails(ids, users)
+  emails = []
+  ids.each do |id|
+    user = users.detect{|u| u.id == id}
+
+    if user.nil?
+      emails << "N/A"
+      next
+    end
+    
+    emails << user.profile.email
+  end
+  emails
+end
+
+def emails_to_users(emails, users)
+  res = []
+  emails.each do |email|
+    user = users.detect{|u| u.profile.email == email}
+
+    if user.nil?
+      next
+    end
+    
+    res << user.clone
+  end
+  res
+end
