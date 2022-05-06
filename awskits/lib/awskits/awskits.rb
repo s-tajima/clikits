0a6ae3b14ccf168b907e64efe7cb8831ce1272be
Satoshi Tajima <tajima1989@gmail.com>
2022-05-06 20:40:26 +0900

    fix awskits

--- awskits/lib/awskits/awskits.rb
+++ awskits/lib/awskits/awskits.rb
@@ -4,25 +4,8 @@ require 'pp'
 
 module AWSKits
   class Factory
-    def initialize(profile, token_code)
-      role_arn       = AWSConfig[profile].role_arn
-      source_profile = AWSConfig[profile].source_profile.name
-      mfa_serial     = AWSConfig[profile].mfa_serial
-      
-      credentials = Aws::SharedCredentials.new(profile_name: source_profile)
-      sts = Aws::STS::Client.new(credentials: credentials)
-      
-      resp = sts.get_session_token({
-        duration_seconds: 900, 
-        serial_number: mfa_serial,
-        token_code: token_code
-      })
-      
-      credentials = Aws::Credentials.new(
-        resp.credentials.access_key_id,
-        resp.credentials.secret_access_key, 
-        resp.credentials.session_token
-      )
+    def initialize(credential_file, role_arn)
+      credentials = Aws::SharedCredentials.new(path: credential_file)
       sts = Aws::STS::Client.new(credentials: credentials)
       
       resp = sts.assume_role({
@@ -38,12 +21,12 @@ module AWSKits
       )
     end
 
-    def client(service)
-      Object.const_get("Aws::#{service}::Client").new(credentials: @credentials)
+    def client(service, region='ap-northeast-1')
+      Object.const_get("Aws::#{service}::Client").new(credentials: @credentials, region: region)
     end
 
     def resource(service)
-      Object.const_get("Aws::#{service}::Resource").new(credentials: @credentials)
+      Object.const_get("Aws::#{service}::Resource").new(credentials: @credentials, region: region)
     end
   end
 end
